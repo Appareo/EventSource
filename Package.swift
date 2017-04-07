28db982ca3e7d96d37e5592d8b71f6f5043b3861
Dakota Smith <dsmith@appareo.com>
2017-04-07 10:59:01 -0700

    spm support

---
+++ Package.swift
@@ -0,0 +1,6 @@
+import PackageDescription
+
+let package = Package(
+	name: "EventSource",
+	exclude: ["EventSource.xcodeproj", "EventSourceExamples", "header.png", "IKEventSource.podspec", "sse-server", "sample.gif"]
+)
